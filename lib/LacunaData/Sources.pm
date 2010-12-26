4af142ca3094dac1f75298bb913fe73edbe57f65
Brad Gilbert <b2gills@gmail.com>
2010-12-26 12:19:16 -0600

    Added some more methods to Sources module

--- lib/LacunaData/Sources.pm
+++ lib/LacunaData/Sources.pm
@@ -6,10 +6,11 @@ use 5.12.2;
 
 use YAML qw'LoadFile';
 use File::Spec::Functions qw'splitpath rel2abs catfile catpath';
+use LWP::Simple;
 
 require Exporter;
 our @ISA = qw'Exporter';
-our @EXPORT_OK = qw'source_of';
+our @EXPORT_OK = qw'source_of file_for get_source';
 
 our $global_config;
 our $path;
@@ -38,5 +39,62 @@ sub source_of{
 
   my($package, $filename, $line) = caller;
   die qq[Unable to find the source of "$source" at $filename line:$line\n];
+}
+
+sub file_for{
+  my($class, $source) = @_;
+  $source = $class unless $class eq __PACKAGE__;
+
+  my $file = rel2abs $global_config->{$source}{file}, $path;
+  return $file if $file;
+
+  my($package, $filename, $line) = caller;
+  die qq[Unable to find the file for "$source" at $filename line:$line\n];
+}
+
+sub url_for{
+  my($class, $source) = @_;
+  $source = $class unless $class eq __PACKAGE__;
+
+  my $url = $global_config->{$source}{url};
+  return $url if $url;
+
+  my($package, $filename, $line) = caller;
+  die qq[Unable to find the url for "$source" at $filename line:$line\n];
+}
+
+sub _read_file{
+  my($filename) = @_;
+  open my $fh, '<', $filename;
+  local $/;
+  my $contents = <$fh>;
+  close $fh;
+  return $contents;
+}
+
+sub get_source{
+  my $class = __PACKAGE__;
+  shift if $_[0] eq $class;
+  my($source, $type) = @_;
+
+  my $uri;
+  if( $type ){
+    given( $type ){
+      $uri = $class->url_for( $source) when 'url';
+      $uri = $class->file_for($source) when 'file';
+    }
+  }else{
+    $uri = $class->source_of($source);
+  }
+
+  given( $uri ){
+    return get $uri when m(^(?: https? | ftp ):// )x;
+    when( m(^file://(.*)) ){
+      return _read_file($1);
+    }
+    default{
+      return _read_file($uri);
+    }
+  }
 }
 1
