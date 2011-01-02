54403d40e20e945edaacf27d1d15f8482e42c21d
Brad Gilbert <b2gills@gmail.com>
2011-01-02 16:27:09 -0600

    Added a script that rebuilds the cache

---
+++ scripts/rebuild_cache.pl
@@ -0,0 +1,23 @@
+#! /usr/bin/env perl
+use strict;
+use warnings;
+use 5.12.2;
+use autodie;
+
+use File::Spec::Functions qw'catdir';
+
+use FindBin;
+use lib "${FindBin::Bin}/../lib";
+
+our $dir = "${FindBin::Bin}/../lib";
+our @modules;
+
+opendir my($dh), catdir($dir,'LacunaData','Load');
+
+while( readdir $dh ){
+  next unless s/\.pm$//;
+  my $module =  "LacunaData::Load::$_";
+  say STDERR 'Loading ', $module;
+  eval "require $module" &&
+  $module->Cache;
+}
