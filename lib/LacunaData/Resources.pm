d3fdd930b681c98bd2ddd3272115d283f0ebe239
Brad Gilbert <b2gills@gmail.com>
2011-01-05 16:01:59 -0600

    Added resource_list to LacunaData::Resources Also replaced Exporter with Sub::Exporter

--- lib/LacunaData/Resources.pm
+++ lib/LacunaData/Resources.pm
@@ -2,12 +2,9 @@ package LacunaData::Resources;
 use strict;
 use warnings;
 
-require Exporter;
-our @ISA = 'Exporter';
-our @EXPORT_OK = qw'ore_list food_list';
-our %EXPORT_TAGS = (
-  all => [@EXPORT_OK]
-);
+use Sub::Exporter -setup => {
+  exports => [ qw'ore_list food_list resource_list' ],
+};
 
 my @ore = qw( anthracite bauxite beryl chalcopyrite chromite fluorite galena goethite gold gypsum halite kerogen magnetite methane monazite rutile sulfur trona uraninite zircon );
 sub ore_list{
@@ -17,4 +14,9 @@ sub ore_list{
 my @food = qw( algae apple bean beetle bread burger cheese chip cider corn fungus lapis meal milk pancake pie potato root shake soup syrup wheat );
 sub food_list{
   @food
+}
+
+my @resource = ( @food, @ore, qw'water energy' );
+sub resource_list{
+  @resource
 }
